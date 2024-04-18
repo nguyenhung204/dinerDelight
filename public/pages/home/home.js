52740f4513ba5187c02bd0e484e2307b8c12cd59
nguyenhung <nguyenhung2004200@gmial.com>
2024-04-18 21:38:35 +0700

    last commit

--- public/pages/home/home.js
+++ public/pages/home/home.js
@@ -1,3 +1,13 @@
+function isMobileDevice() {
+  const userAgent = navigator.userAgent;
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+}
+
+if (isMobileDevice()) {
+  // Chặn truy cập
+  window.location.href = 'https://www.example.com/blocked'; // Thay thế bằng trang web chặn
+}
+
 window.onload = function() {
     if (localStorage.getItem('needTestLogin') === 'true') {
       testLogin();
--- public/shared/js/nav.js
+++ public/shared/js/nav.js
@@ -1,5 +1,5 @@
-let isLogin = false;
 
+let isLogin = false;
 //loading Pages
 function showLoading() {
   let element = document.getElementById("loading-panel");
